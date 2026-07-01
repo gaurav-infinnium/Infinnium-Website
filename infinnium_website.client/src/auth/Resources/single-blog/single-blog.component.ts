1317a28395a4afd705520882211002e710a53761
Gaurav Sanchania <gsanchania@infinniumtech.onmicrosoft.com>
2026-07-01 09:38:43 +0530

    Fixes: recent-blogs > implemented dynamic date instead of static || single resource route > implemented subscription for route changes on same component.

--- infinnium_website.client/src/auth/Resources/single-blog/single-blog.component.ts
+++ infinnium_website.client/src/auth/Resources/single-blog/single-blog.component.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { NewsService } from '../../../services/newsService.service';
 import { BlogsService } from '../../../services/blogsService.service';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -14,11 +15,12 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './single-blog.component.html',
   styleUrl: './single-blog.component.css',
 })
-export class SingleBlogComponent implements OnInit {
+export class SingleBlogComponent implements OnInit, OnDestroy {
   blog: any = [];
   news: any = [];
   public guid = '';
   isContentLoaded = false;
+  private routeSub!: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -28,36 +30,32 @@ export class SingleBlogComponent implements OnInit {
     private sanitizer: DomSanitizer
   ) {}
 
-  async ngOnInit() {
-    this.isContentLoaded = false;
-    const guidFromRoute = this.route.snapshot.paramMap.get('guid');
-    // console.log(guidFromRoute);
-    if (guidFromRoute) {
-      // console.log("Inside If");
-      const url = this.router.url;
-      this.guid = guidFromRoute;
-      //console.log(url);
-
-      if (url.startsWith('/resources/blogs')) {
-        this.blog = await this.blogService.getBlogDetails(this.guid);
-        this.blog.description = this.blog.description.replace(/&nbsp;/g, ' ');
-        this.blog.description = this.sanitizer.bypassSecurityTrustHtml(
-          this.blog.description
-        );
-        //console.log(this.blog);
-
-      } else {
-        this.news = await this.newsService.getNewsDetails(this.guid);
-        this.news.description = this.news.description.replace(/&nbsp;/g, ' ');
-        this.news.description = this.sanitizer.bypassSecurityTrustHtml(
-          this.news.description
-        );
-        //console.log(this.news);
+  ngOnInit() {
+    this.routeSub = this.route.paramMap.subscribe(params => {
+      const guidFromRoute = params.get('guid');
+      if (guidFromRoute) {
+        this.guid = guidFromRoute;
+        this.loadContent();
       }
-      this.isContentLoaded = true;
+    });
+  }
+
+  async loadContent() {
+    this.isContentLoaded = false;
+    const url = this.router.url;
+    if (url.startsWith('/resources/blogs')) {
+      this.blog = await this.blogService.getBlogDetails(this.guid);
+      this.blog.description = this.blog.description.replace(/&nbsp;/g, ' ');
+      this.blog.description = this.sanitizer.bypassSecurityTrustHtml(this.blog.description);
     } else {
-      //console.error('GUID not found in route!');
-      return;
+      this.news = await this.newsService.getNewsDetails(this.guid);
+      this.news.description = this.news.description.replace(/&nbsp;/g, ' ');
+      this.news.description = this.sanitizer.bypassSecurityTrustHtml(this.news.description);
     }
+    this.isContentLoaded = true;
+  }
+
+  ngOnDestroy() {
+    this.routeSub?.unsubscribe();
   }
 }
